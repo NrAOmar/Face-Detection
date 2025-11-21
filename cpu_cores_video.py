b6983c6806290567d0f5d7c81a7df35eefd2e264
NrAOmar <omarfahmy1006@gmail.com>
2025-11-21 17:56:10 +0100

    working cpu_cores_video but not what we want

--- cpu_cores_video.py
+++ cpu_cores_video.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from concurrent.futures import ThreadPoolExecutor
 from numba import njit, prange
+import helpers
 
 # Example numba-accelerated operation
 @njit(parallel=True)
@@ -13,8 +14,9 @@ def brighten(frame):
     return frame
 
 # Other operations (OpenCV built-in = already fast)
-def to_gray(frame):
-    return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def to_gray(frame, angle):
+    frame_rotated, rot_mat = helpers.rotate_image_without_cropping(frame, angle)
+    return frame_rotated
 
 def edges(frame):
     return cv2.Canny(frame, 120, 200)
@@ -30,18 +32,21 @@ while True:
 
     # Submit parallel tasks
     f1 = executor.submit(brighten, frame.copy())
-    f2 = executor.submit(to_gray, frame.copy())
-    f3 = executor.submit(edges, frame.copy())
+    f2 = executor.submit(edges, frame.copy())
+    f3 = executor.submit(to_gray, frame.copy(), 45)
+    f4 = executor.submit(to_gray, frame.copy(), 90)
 
     # Wait for all results
     bright_frame = f1.result()
-    gray_frame   = f2.result()
-    edges_frame  = f3.result()
+    edges_frame  = f2.result()
+    gray_frame   = f3.result()
+    gray_frame2   = f4.result()
 
     # Display all results
     cv2.imshow("Original", frame)
     cv2.imshow("Brightened", bright_frame)
     cv2.imshow("Gray", gray_frame)
+    cv2.imshow("Gray2", gray_frame2)
     cv2.imshow("Edges", edges_frame)
 
     if cv2.waitKey(1) == 27:  # ESC to exit
